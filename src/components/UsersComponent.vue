c63f3db28db989c48ab1ab2b32be6afbf871e502
sevgiguclu <guclusevgi97@gmail.com>
2025-04-08 10:16:11 +0300

    added dialog for update user

--- src/components/UsersComponent.vue
+++ src/components/UsersComponent.vue
@@ -2,172 +2,172 @@
   <v-container>
     <h2>User Table</h2>
 
-    <!-- dialog start -->
+    <!-- add user dialog start -->
     <div>
       <v-dialog
-        v-model="dialog"
+        v-model="addUserDialog"
         persistent
         max-width="600px"
       >
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn 
-          class="my-3" 
-          color="primary"
-          elevation="1" 
-          rounded
-          v-bind="attrs"
-          v-on="on"
-          @click="openAddUserDialog"
-        >
-          Create User
-        </v-btn>
-      </template>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn 
+            class="my-3" 
+            color="primary"
+            elevation="1" 
+            rounded
+            v-bind="attrs"
+            v-on="on"
+            @click="openAddUserDialog"
+          >
+            Create User
+          </v-btn>
+        </template>
       
-      <v-card>
-        <v-card-title>
-          <span class="text-h5">User Create</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col
-                cols="12"
-              >
-                <v-text-field
-                  label="Name*"
-                  v-model="userOne.name"
-                  required
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col
-                cols="12"
-              >
-                <v-text-field
-                  label="E-mail"
-                  v-model="userOne.email"
-                  hint="Your email must include @"
-                  :rules="emailRules"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col
-                cols="12"
-                sm="6"
-              >
-                <v-select
-                  :items="['0-17', '18-29', '30-54', '54+']"
-                  v-model="userOne.age"
-                  label="Age"
-                ></v-select>
-              </v-col>
-              <v-col
-                cols="12"
-                sm="6"
-              >
-                <v-autocomplete
-                  :items="companyList"
-                  item-text="name"
-                  item-value="_id"
-                  label="Company*"
-                  v-model="userOne.company"
-                  required
-                ></v-autocomplete>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col
-                cols="12"
-              >
-                <v-text-field
-                  label="Job"
-                  v-model="userOne.job"
-                ></v-text-field>
-              </v-col>
-            </v-row>
+        <v-card>
+          <v-card-title>
+            <span class="text-h5">User Create</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col
+                  cols="12"
+                >
+                  <v-text-field
+                    label="Name*"
+                    v-model="userOne.name"
+                    required
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col
+                  cols="12"
+                >
+                  <v-text-field
+                    label="E-mail"
+                    v-model="userOne.email"
+                    hint="Your email must include @"
+                    :rules="emailRules"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col
+                  cols="12"
+                  sm="6"
+                >
+                  <v-select
+                    :items="['0-17', '18-29', '30-54', '54+']"
+                    v-model="userOne.age"
+                    label="Age"
+                  ></v-select>
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                >
+                  <v-autocomplete
+                    :items="companyList"
+                    item-text="name"
+                    item-value="_id"
+                    label="Company*"
+                    v-model="userOne.company"
+                    required
+                  ></v-autocomplete>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col
+                  cols="12"
+                >
+                  <v-text-field
+                    label="Job"
+                    v-model="userOne.job"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
 
 
-            <!-- address start-->
-            <!-- <v-row>
-              <v-col col="12">
-                <v-text-field 
-                  label="Adress Name"
-                  v-model="userOne.address.name"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col col="12">
-                <v-text-field 
-                  label="Adress"
-                  v-model="userOne.address.value"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col
-                col="12"
-              >
-                <v-text-field
-                  label="Adress City"
-                  v-model="userOne.address.city"
-                ></v-text-field>
-              </v-col>
-            </v-row> -->
-            <!-- address end -->
-                
-            <v-row v-show="addressSelectVisibility">
-              <v-col col="12">
-                <v-autocomplete
-                  :items="addressList"
-                  item-text="name"
-                  item-value="_id"
-                  label="Address"
-                  v-model="userOne.address"
-                ></v-autocomplete>
-              </v-col>
-            </v-row>
+              <!-- address start-->
+              <!-- <v-row>
+                <v-col col="12">
+                  <v-text-field 
+                    label="Adress Name"
+                    v-model="userOne.address.name"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col col="12">
+                  <v-text-field 
+                    label="Adress"
+                    v-model="userOne.address.value"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col
+                  col="12"
+                >
+                  <v-text-field
+                    label="Adress City"
+                    v-model="userOne.address.city"
+                  ></v-text-field>
+                </v-col>
+              </v-row> -->
+              <!-- address end -->
+                  
+              <v-row v-show="addressSelectVisibility">
+                <v-col col="12">
+                  <v-autocomplete
+                    :items="addressList"
+                    item-text="name"
+                    item-value="_id"
+                    label="Address"
+                    v-model="userOne.address"
+                  ></v-autocomplete>
+                </v-col>
+              </v-row>
 
-            <v-row>
-              <v-col
-                col="12"
-              >
-                <v-btn 
-                @click="addressSelectVisibility= true;addAddressBtnVisibility=false;"
-                v-show="addAddressBtnVisibility">
-                  Add Address
-                </v-btn>
-              </v-col>
-            </v-row> 
+              <v-row>
+                <v-col
+                  col="12"
+                >
+                  <v-btn 
+                  @click="addressSelectVisibility= true;addAddressBtnVisibility=false;"
+                  v-show="addAddressBtnVisibility">
+                    Add Address
+                  </v-btn>
+                </v-col>
+              </v-row> 
 
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="dialog = false"
-          >
-            Close
-          </v-btn>
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="addUser"
-          >
-            Save
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+            </v-container>
+            <small>*indicates required field</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="blue darken-1"
+              text
+              @click="addUserDialog = false"
+            >
+              Close
+            </v-btn>
+            <v-btn
+              color="blue darken-1"
+              text
+              @click="addUser"
+            >
+              Save
+            </v-btn>
+          </v-card-actions>
+        </v-card>
 
 
       </v-dialog>
     </div>
-    <!-- dialog end -->
+    <!-- add user dialog end -->
 
 
     <v-simple-table>
@@ -188,9 +188,134 @@
               {{ item.email }}
             </td>
             <td>
-              <v-btn icon color="primary" @click="updateUser(item._id)">
+
+              <!-- <v-btn icon color="primary" @click="updateUser(item._id)">
                 <v-icon> mdi-pencil </v-icon>
-              </v-btn>
+              </v-btn> -->
+
+              <v-dialog 
+                v-model="updateUserDialog"
+                persistent
+                max-width="600px"
+                :retain-focus="false"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn 
+                      class="my-3" 
+                      color="primary" 
+                      icon
+                      v-bind="attrs"
+                      v-on="on"
+                      @click="openUpdateUserDialog(item._id)"
+                    >
+                      <v-icon> mdi-pencil </v-icon>
+                    </v-btn>
+                </template>
+
+                <v-card>
+                  <v-card-title>
+                    <span class="text-h5">Update User</span>
+                  </v-card-title>
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col
+                          cols="12"
+                        >
+                          <v-text-field
+                            label="Name*"
+                            v-model="userOne.name"
+                            required
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col
+                          cols="12"
+                        >
+                          <v-text-field
+                            label="E-mail"
+                            v-model="userOne.email"
+                            hint="Your email must include @"
+                            :rules="emailRules"
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                        >
+                          <v-select
+                            :items="['0-17', '18-29', '30-54', '54+']"
+                            v-model="userOne.age"
+                            label="Age"
+                          ></v-select>
+                        </v-col>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                        >
+                          <v-autocomplete
+                            :items="companyList"
+                            item-text="name"
+                            item-value="_id"
+                            label="Company*"
+                            v-model="userOne.company"
+                            required
+                          ></v-autocomplete>
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col
+                          cols="12"
+                        >
+                          <v-text-field
+                            label="Job"
+                            v-model="userOne.job"
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+
+                          
+                      <v-row>
+                        <v-col col="12">
+                          <v-autocomplete
+                            :items="addressList"
+                            item-text="name"
+                            item-value="_id"
+                            label="Address"
+                            v-model="userOne.address"
+                          ></v-autocomplete>
+                        </v-col>
+                      </v-row> 
+
+                    </v-container>
+                    <small>*indicates required field</small>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      color="blue darken-1"
+                      text
+                      @click="updateUserDialog = false;"
+                    >
+                      Close
+                    </v-btn>
+                    <v-btn
+                      color="blue darken-1"
+                      text
+                      @click="updateUser()"
+                    >
+                      Save
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+
+
+              </v-dialog>
+
+
               <v-btn icon color="red" @click="deleteUser(item._id)">
                 <v-icon> mdi-delete </v-icon>
               </v-btn>
@@ -220,7 +345,8 @@ export default {
       emailRules : [
         v => /.+@.+/.test(v) || 'E-mail must be valid',
       ],
-      dialog: false,
+      addUserDialog: false,
+      updateUserDialog : false,
       addressSelectVisibility:false,
       addAddressBtnVisibility:true
     };
@@ -232,6 +358,7 @@ export default {
       .then((response) => (this.userList = response.data));
     },
     openAddUserDialog: async function () {
+        this.userOne = {};
         await axios
         .get("http://localhost:8000/companies")
         .then((response) => this.companyList = response.data)
@@ -239,7 +366,7 @@ export default {
 
         await axios
         .get("http://localhost:8000/addresses")
-      . then((response) => (this.addressList = response.data));
+        .then((response) => (this.addressList = response.data));
 
     },
     addUser: async function(){
@@ -260,8 +387,33 @@ export default {
 
 
     },
-    updateUser: function (id) {
-      this.$router.push("/users/updateuser/" + id);
+    openUpdateUserDialog: async function(id){
+      await axios
+      .get("http://localhost:8000/users/finduserbyid/"+id)
+      .then((response) => (this.userOne = response.data));
+      await axios
+        .get("http://localhost:8000/companies")
+        .then((response) => this.companyList = response.data)
+        .catch((error) => console.log(error));
+
+        await axios
+        .get("http://localhost:8000/addresses")
+        .then((response) => (this.addressList = response.data));
+    },
+    updateUser: async function () {
+      // this.$router.push("/users/updateuser/" + id);
+      // console.log(this.userOne);
+      await axios
+      .patch('http://localhost:8000/users/updateuser/' + this.userOne._id,this.userOne)
+      .then(response => {
+          console.log(response.data);
+          alert("user update");
+      })
+      .catch(function (error) {
+          console.log(error);
+          alert("error.. :( )")
+      });
+      
     },
     deleteUser: async function (id) {
       await axios
